e826a1dca99d7d0e587d4cab1251640e671490cb
VildanYakupov <Vildandev@yandex.ru>
2022-07-04 16:19:22 +0300

    update file;

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 // *подключение плагинов;
 const gulp = require('gulp');
-// const browserSync = require('browserSync').create();
-// const del = require('del');
+const browsersync = require('browser-sync').create();
+const del = require('del');
 const scss = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const fileInclude = require('gulp-file-include');
+const uglify = require('gulp-uglify');
 
 // *переменные к папке;
 let distFolder = "dist";
@@ -18,7 +20,7 @@ const paths = {
     src: {
         html: srcFolder + "/index.html",
         css: srcFolder + "/scss/style.scss",
-        scripts: srcFolder + "/scripts/**/*.js",
+        scripts: srcFolder + "/scripts/script.js",
         fonts: srcFolder + "/fonts/**/*.ttf",
         img: srcFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     },
@@ -36,7 +38,14 @@ const paths = {
         scripts: srcFolder + "/js/**/*.js",
         img: srcFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     },
-    clean: "./" + distFolder +"/",
+    distClean: distFolder +"/",
+}
+
+function changeHTML() {
+    return gulp.src(paths.src.html)
+        .pipe(fileInclude())
+        .pipe(gulp.dest(paths.dest.html))
+        .pipe(browsersync.stream())
 }
 
 function changeStyles() {
@@ -52,29 +61,53 @@ function changeStyles() {
                 extname: '.css'
             }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.dest.css));
+        .pipe(gulp.dest(paths.dest.css))
+        .pipe(browsersync.stream())
 }
 
-function changeHTML() {
-    return gulp.src(paths.src.html)
-        .pipe(gulp.dest(paths.dest.html));
+function changeScripts() {
+    return gulp.src(paths.src.scripts)
+        .pipe(sourcemaps.init())
+            .pipe(fileInclude())
+            .pipe(gulp.dest(paths.dest.scripts))
+            .pipe(uglify())
+            .pipe(rename({
+                basename: 'script',
+                suffix: '.min',
+                extname: '.js'
+            }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.dest.scripts))
+        .pipe(browsersync.stream())
 }
 
-// function browsersync() {
-//     browserSync.init({
-//         server:{
-//             baseDir: './' + distFolder + "/"
-//         },
-//     })
-// }
+
+function browserSync() {
+    browsersync.init({
+        server:{
+            baseDir: distFolder +"/",
+        },
+    })
+}
 
 function watchFiles() {
     gulp.watch(paths.watch.html, changeHTML),
-    gulp.watch(paths.watch.css, changeStyles)
+    gulp.watch(paths.watch.css, changeStyles),
+    gulp.watch(paths.watch.css, changeScripts)
 }
 
+function cleanFolder() {
+    return del(paths.distClean);
+}
 
+let build = gulp.series(cleanFolder, gulp.parallel(changeHTML, changeStyles, changeScripts))
+let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.build = build;
+exports.watch = watch;
+exports.browserSync = browserSync;
 exports.watchFiles = watchFiles;
 exports.changeHTML = changeHTML;
 exports.changeStyles = changeStyles;
+exports.changeScripts = changeScripts;
+exports.cleanFolder = cleanFolder;
